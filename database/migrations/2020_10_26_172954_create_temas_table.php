5f5250fdf8e3e0d3da1add1c22844aec51416aad
victor27017 <chumacero270@gmail.com>
2020-10-26 14:32:12 -0400

    Autenticacion y npm

---
+++ database/migrations/2020_10_26_172954_create_temas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temas');
+    }
+}
